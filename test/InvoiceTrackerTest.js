e123ef54070e9449e3be545763c74fe8456d794e
Denis Putnam <denisputnam@verizon.net>
2020-04-28 14:27:19 -0400

    Got 'javascript test add a client' to work.

--- test/InvoiceTrackerTest.js
+++ test/InvoiceTrackerTest.js
@@ -11,8 +11,14 @@ contract("InvoiceTracker", async accounts => {
 
   it('javascript test add a client', async () => {
     let clientID = "0x874390a3787ef36bcd255de00f47f2dc34f70d95";
-    const results = await invoiceTracker.addClient(clientID,"test");
-    truffleAssert.prettyPrintEmittedEvents(results);
-    assert.isTrue(true);
+    const result = await invoiceTracker.addClient(clientID,"test");
+    truffleAssert.prettyPrintEmittedEvents(result);
+    truffleAssert.eventEmitted(result, 'addClientEvent', (event) => {
+      console.log("event._clientID=" + event._clientID.toUpperCase());
+      console.log("       clientID=" + clientID.toUpperCase());
+      const myequal = event._clientID.toUpperCase() === clientID.toUpperCase();
+      console.log("DEBUG:" + myequal);
+      return event._clientID.toUpperCase() == clientID.toUpperCase() && event._name === "test";
+    });
   });
 });
