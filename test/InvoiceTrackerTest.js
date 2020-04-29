175cd2491a038ea68ed5fa0b247d2dcb7a6c19e8
Denis Putnam <denisputnam@verizon.net>
2020-04-29 11:48:17 -0400

    Moved test add a client logic into the beforeEach() method to ensure that a client is established due to the async nature of the tests.

--- test/InvoiceTrackerTest.js
+++ test/InvoiceTrackerTest.js
@@ -7,9 +7,6 @@ contract("InvoiceTracker", async accounts => {
   let invoiceTracker;
   beforeEach(async () => {
     invoiceTracker = await InvoiceTracker.new();
-  });
-
-  it('javascript test add a client', async () => {
     let clientID = "0x874390a3787ef36bcd255de00f47f2dc34f70d95";
     const result = await invoiceTracker.addClient(clientID, "test");
     truffleAssert.prettyPrintEmittedEvents(result);
@@ -21,6 +18,19 @@ contract("InvoiceTracker", async accounts => {
       return event._clientID.toUpperCase() == clientID.toUpperCase() && event._name === "test";
     });
   });
+
+  // it('javascript test add a client', async () => {
+  //   let clientID = "0x874390a3787ef36bcd255de00f47f2dc34f70d95";
+  //   const result = await invoiceTracker.addClient(clientID, "test");
+  //   truffleAssert.prettyPrintEmittedEvents(result);
+  //   truffleAssert.eventEmitted(result, 'addClientEvent', (event) => {
+  //     console.log("event._clientID=" + event._clientID.toUpperCase());
+  //     console.log("       clientID=" + clientID.toUpperCase());
+  //     const myequal = event._clientID.toUpperCase() === clientID.toUpperCase();
+  //     console.log("DEBUG:" + myequal);
+  //     return event._clientID.toUpperCase() == clientID.toUpperCase() && event._name === "test";
+  //   });
+  // });
 
   it('javascript test add an invoice', async () => {
     let clientID = "0x874390a3787ef36bcd255de00f47f2dc34f70d95";
